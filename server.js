7ad05dcf9d966ef8887c51224ba707585d3cd404
Paulina Broni <pbroni@myseneca.ca>
2022-09-14 16:07:06 -0400

    commit changes

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: ______________________ Student ID: ______________ Date: ________________
+* Cyclic Link: _______________________________________________________________
+*
+********************************************************************************/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+var cors = require("cors");
+var dotenv = require("dotenv").config();
+var mongoose = require("mongoose");
 
+app.use(cors());
+app.use(express.json());
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
     res.json({message:"API Listening"});

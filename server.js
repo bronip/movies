93c8d9020b359c44ac1a48a255ed599ee9fa7f71
Paulina Broni <pbroni@myseneca.ca>
2022-09-15 12:26:17 -0400

    commit changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
 *
-* Name: ______________________ Student ID: ______________ Date: ________________
+* Name: Paulina Broni Student ID: 154548192 Date: Thursday, September 15, 2022
 * Cyclic Link: _______________________________________________________________
 *
 ********************************************************************************/
@@ -14,7 +14,10 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 var dotenv = require("dotenv").config();
-var mongoose = require("mongoose");
+const { MONGO_CONNECT_STRING } = process.env;
+const MoviesDB = require("./modules/moviesDB.js");
+const db = new MoviesDB();
+//var mongoose = require("mongoose");
 
 app.use(cors());
 app.use(express.json());
@@ -23,5 +26,63 @@ app.get("/", (req, res) => {
     res.json({message:"API Listening"});
 });
 
+
+app.post("/api/movies", function(req,res){
+
+    db.addNewMovie(req.body)
+    .then((data)=>{
+        res.json(data);
+   
+})
+.catch((err) => {
+    res.status(400).json(err);
+    });
+});
+
+app.get("/api/movies", (req,res)=>{
+    db.getAllMovies(req.query.page, req.query.perPage, req.query.title)
+    .then(data=>{
+        res.json(data);
+    })
+    .catch((err)=>{
+        res.status(500).json({message:err.message});
+    })
+ });
+
+app.get("/api/movies/:id", function(req,res){
+ db.getMovieById(req.params.id).then(data=>{
+    res.json(data);
+ })
+ .catch((err)=>{
+    res.status(500).json({message:err.message});
+    })
+});
+
+app.put("/api/movie/:id", function(req,res){
+db.updateMovieById(req.body, req.params.id)
+.then(() => {
+    res.status(200).json(`movie ${req.body._id} successfully updated`);
+})
+.catch((err) => {
+    res.status(404).json(err);
+});
+});
+
+app.delete("/api/movies/:id", function(req,res){
+    myData.deleteMovieById(req.params.id)
+    .then(() => {
+        res.status(200).json(`movie ${req.params.id} successfully deleted`);
+    })
+    .catch((err) => {
+        res.status(404).json(err);
+    });
+});
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+
+db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
+    app.listen(HTTP_PORT, ()=>{
+    console.log(`server listening on: ${HTTP_PORT}`);
+    });
+    }).catch((err)=>{
+    console.log(err);
+    });
